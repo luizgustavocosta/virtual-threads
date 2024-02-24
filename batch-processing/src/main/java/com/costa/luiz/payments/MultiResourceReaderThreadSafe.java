2a32eb1808dd25ab82a30238b703ffa02a1425b9
Luiz Gustavo Costa <luiz.gustavo.costa82@gmail.com>
2024-02-24 01:38:30 +0100

    Batching project

---
+++ batch-processing/src/main/java/com/costa/luiz/payments/MultiResourceReaderThreadSafe.java
@@ -0,0 +1,41 @@
+package com.costa.luiz.payments;
+
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.MultiResourceItemReader;
+import org.springframework.core.io.Resource;
+
+public class MultiResourceReaderThreadSafe<T> implements ItemReader<T> {
+
+    private final MultiResourceItemReader<T> delegate;
+    private final Object lock = new Object();
+
+    public MultiResourceReaderThreadSafe(MultiResourceItemReader<T> delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public T read() throws Exception {
+        synchronized (lock) {
+            return delegate.read();
+        }
+    }
+
+    public void setResources(Resource[] resources) {
+        synchronized (lock) {
+            delegate.setResources(resources);
+        }
+    }
+
+    public void open(ExecutionContext executionContext) {
+        synchronized (lock) {
+            delegate.open(executionContext);
+        }
+    }
+
+    public void close() {
+        synchronized (lock) {
+            delegate.close();
+        }
+    }
+}
